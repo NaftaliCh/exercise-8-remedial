1f0d3148db69bdf6f8e2435c15e2e1dbc44b4ff5
Kerry Pasuhuk <S2200567@student.unklab.ac.id>
2023-12-06 15:03:47 +0800

    component homepage

---
+++ src/page/homepage.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header/index"
+import Home from "../components/Home/index"
+import Footer from "../components/Footer/index"
+import (Header)
+
+const homepage = () => {
+  return (
+    <div>
+        <Header/>
+        <Home/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default homepage
